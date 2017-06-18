f79d9848e5493ab9e1f86c4666549dab627debe0
Kevia Cloud <cloud@kevia.me>
2017-06-18 15:49:42 -0700

    Playing around with some new colors

--- src/js/comps/App.jsx
+++ src/js/comps/App.jsx
@@ -4,9 +4,19 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <main id="main">
+        <section className="row profile">
+          <div className="col s12">
+            <p>Hello, my name is...</p>
+            <h1>Kevia Cloud</h1>
+          </div>
+        </section>
+
+        <section className="row other">
+          <div className="col s3 hobbies"></div>
+          <div className="col s9"></div>
+        </section>
+      </main>
     );
   }
 
