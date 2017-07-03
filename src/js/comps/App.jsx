2228a11bbe6b257917da1dd9190e1b6601f7815e
Kevia Cloud <cloud@kevia.me>
2017-07-03 13:48:13 -0700

    Little better

--- src/js/comps/App.jsx
+++ src/js/comps/App.jsx
@@ -5,17 +5,12 @@ export default class App extends React.Component {
   render() {
     return (
       <main id="main">
-        <section className="row profile">
-          <div className="col s12">
-            <p>Hello, my name is...</p>
-            <h1>Kevia Cloud</h1>
-          </div>
-        </section>
-
-        <section className="row other">
-          <div className="col s3 hobbies"></div>
-          <div className="col s9"></div>
-        </section>
+        <div className="text-box">
+          <h1>Kevia Cloud</h1>
+          <p>Front End Developer</p>
+          <p>Scottsdale, AZ</p>
+        </div>
+        <div className="purple-filter"></div>
       </main>
     )
   }
