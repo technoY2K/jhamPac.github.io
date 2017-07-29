32cad561342d577dbeb8c219206e1fc6b4b60c52
Kevia Cloud <cloud@kevia.me>
2017-07-28 22:58:29 -0700

    Added back in semi colons, I like snake_case variables

--- src/js/comps/App.jsx
+++ src/js/comps/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default class App extends React.Component {
 
@@ -40,7 +40,7 @@ export default class App extends React.Component {
           </ul>
         </div>
       </main>
-    )
+    );
   }
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 // Dependencies
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const extractCSS = new ExtractTextPlugin('css/[hash:4].css')
-const extractSCSS = new ExtractTextPlugin('css/[hash:4].css')
+const PATH = require('path');
+const CSS_TEXT_EXTRACT = new ExtractTextPlugin('css/[hash:4].css');
+const SCSS_TEXT_EXTRACT = new ExtractTextPlugin('css/[hash:4].css');
+let Webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Webpack variables
-let isProduction = process.env.NODE_ENV === 'production'
-let cssDev = ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, { loader: 'postcss-loader', options: { sourceMap: true } }]
-let cssProd = ExtractTextPlugin.extract( { fallback: 'style-loader', use: [{ loader: 'css-loader', options: { importLoaders: 1 } }, { loader: 'postcss-loader', options: { sourceMap: true } }] } )
-let scssDev = ['style-loader', 'css-loader', 'resolve-url-loader', {loader: 'sass-loader', options: { sourceMap: true }}, 'import-glob-loader' ]
-let scssProd = ExtractTextPlugin.extract({
+let is_production = process.env.NODE_ENV === 'production';
+
+let development_css = ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, { loader: 'postcss-loader', options: { sourceMap: true } }];
+
+let production_css = ExtractTextPlugin.extract( { fallback: 'style-loader', use: [{ loader: 'css-loader', options: { importLoaders: 1 } }, { loader: 'postcss-loader', options: { sourceMap: true } }] } );
+
+let development_scss = ['style-loader', 'css-loader', 'resolve-url-loader', {loader: 'sass-loader', options: { sourceMap: true }}, 'import-glob-loader' ];
+
+let production_scss = ExtractTextPlugin.extract({
   fallback: 'style-loader',
   use: ['css-loader', 'resolve-url-loader', {loader: 'sass-loader', options: { sourceMap: true }}, 'import-glob-loader' ]
-})
+});
 
-const DISTPATH  = path.join(__dirname, '/')
-const ENTRYFILE = path.join(__dirname, 'src/main.js')
+const DISTPATH  = PATH.join(__dirname, '/');
+const ENTRYFILE = PATH.join(__dirname, 'src/main.js');
 
 const RULES = [
 
@@ -31,33 +35,33 @@ const RULES = [
     }
   },
 
-  { test: /\.css$/, use: (isProduction) ? cssProd : cssDev  },
+  { test: /\.css$/, use: (is_production) ? production_css : development_css  },
 
-  { test: /\.scss$/, use: (isProduction) ? scssProd : scssDev },
+  { test: /\.scss$/, use: (is_production) ? production_scss : development_scss },
 
   { test: /\.(png|gif|jpg|svg)$/, use: [{loader: 'file-loader', options: {name: 'images/[name].[ext]'}}, {loader: 'image-webpack-loader'}] },
 
   { test: /\.(eot|ttf|woff|woff2)$/, use: [{loader: 'url-loader', options: {limit: '10000', name: 'fonts/[name].[ext]'}}] }
-]
+];
 
-let PLUGINS = [
+const PLUGINS = [
   new HtmlWebpackPlugin({
     minify: {collapseWhitespace: false},
     template: './src/index.html'
   }),
 
-  new webpack.NamedModulesPlugin(),
+  new Webpack.NamedModulesPlugin(),
 
   new ExtractTextPlugin({
-    disable: !isProduction,
+    disable: !is_production,
     filename: 'css/[name].css',
     allChunks: true
   }),
 
-]
+];
 
-if ( !isProduction ) {
-  PLUGINS.push(new webpack.HotModuleReplacementPlugin())
+if ( !is_production ) {
+  PLUGINS.push(new Webpack.HotModuleReplacementPlugin());
 }
 
 // Webpack settings start here
@@ -67,7 +71,7 @@ module.exports = {
 
   entry: {
     // we don't need dev server stuff for production builds.
-    main: isProduction ? ENTRYFILE : [
+    main: is_production ? ENTRYFILE : [
       'webpack-dev-server/client?http://localhost:8080',
       'webpack/hot/only-dev-server',
       ENTRYFILE
@@ -76,11 +80,11 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.scss'],
-    modules: [path.resolve('./node_modules')],
+    modules: [PATH.resolve('./node_modules')],
     alias: {
-      Comps: path.resolve(__dirname, './src/js/comps'),
-      Images: path.resolve(__dirname, './src/images'),
-      Utils: path.resolve(__dirname, './src/js/utils')
+      Comps: PATH.resolve(__dirname, './src/js/comps'),
+      Images: PATH.resolve(__dirname, './src/images'),
+      Utils: PATH.resolve(__dirname, './src/js/utils')
     }
   },
 
@@ -95,7 +99,7 @@ module.exports = {
   },
 
   // build speed plz
-  devtool: isProduction ? 'cheap-module-source-map' : 'source-map',
+  devtool: is_production ? 'cheap-module-source-map' : 'source-map',
 
   devServer: {
     contentBase: DISTPATH,
