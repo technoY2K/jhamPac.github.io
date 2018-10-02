9744a1583782007576974546f6dfc6836f99a08d
Cloud <cloud@kevia.me>
2018-10-01 18:38:02 -0700

    remove links

--- src/js/comps/index.js
+++ src/js/comps/index.js
@@ -9,23 +9,6 @@ export default class App extends Component {
         <section className="nameBox">
           <h1>Tesla VIP Services</h1>
           <p>coming soon...</p>
-          <div>
-
-          </div>
-          <ul className="social-box">
-            <li>
-              <a className="btn-floating btn-large waves-effect waves-light social-media-icon" href="https://www.linkedin.com/in/kevia-cloud" target="_blank" style={ { backgroundImage: `url(${styles.linkedIn})` } }>
-              </a>
-            </li>
-            <li>
-              <a className="btn-floating btn-large waves-effect waves-light social-media-icon" href="https://twitter.com/therealcloud__" target="_blank" style={ { backgroundImage: `url(${styles.twitter})` } }>
-              </a>
-            </li>
-            <li>
-              <a className="btn-floating btn-large waves-effect waves-light social-media-icon" href="https://github.com/jhamPac" target="_blank" style={ { backgroundImage: `url(${styles.github})` } }>
-              </a>
-            </li>
-          </ul>
         </section>
       </main>
     );
