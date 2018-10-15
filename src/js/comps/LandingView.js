e07e31cd0e8ea539c1bf285fc83ad9c10cf699b3
Cloud <cloud@kevia.me>
2018-10-14 17:58:01 -0700

    vehichle => vehicle;

--- src/js/comps/LandingView.js
+++ src/js/comps/LandingView.js
@@ -59,7 +59,7 @@ export default function LandingView(props) {
   return(
     <main id="landing-view" className="row">
       <Hero>
-        <Headline title="Electric Saguaro" tagline="Luxury (⚡️EV) electric vehichle rentals" />
+        <Headline title="Electric Saguaro" tagline="Luxury (⚡️EV) electric vehicle rentals" />
       </Hero>
 
       <TextBox message="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.">
