3337d0a04f969d37f7435a6d237e505d1237f5be
Cloud <cloud@kevia.me>
2018-12-01 23:20:51 -0700

    laying some simple cards down

---
+++ src/comps/Application.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Application extends Component {
+  render() {
+    return(
+      <main className="container">
+        <div className="row">
+          <div className="col s12">
+            <div className="card">
+              <div className="card-content">
+                <p>Hello, introduction here</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
