b77bf27b9814e34f843dea17fd286754172d476d
jhamPac <cloud@kevia.me>
2018-12-08 18:48:09 -0700

    its coming along

--- src/comps/Application.js
+++ src/comps/Application.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
+import imgSrc from 'images/hero_background.jpg'
+
 export default class Application extends Component {
   render() {
     return(
       <main id="UI">
-        <div className="row">
+        <nav className="row">
           <div className="col s12">
-            <h1 className="center-align">Kevia</h1>
+            
+          </div>
+        </nav>
+        <div className="row top-half" style={{ backgroundImage: `url(${imgSrc})` }}>
+          <div className="tint-box"></div>
+          <div className="col s12 text-box">
+            <p className="title-name">Kevia Cloud</p>
+            <h3>Software Engineer</h3>
+            <div className="divider"></div>
+          </div>
+        </div>
+        <div className="row nomargin bottom-half">
+          <div className="col s12 m6 bio">
+            <p>asdfasf</p>
+          </div>
+          <div className="col s12 m6 contact">
+
           </div>
         </div>
       </main>
