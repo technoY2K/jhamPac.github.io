bc2bcf0c9e5aca1eb9ef7eac092b65a9ef813483
jhamPac <cloud@kevia.me>
2018-12-15 22:12:55 -0700

    save point

--- src/comps/Application.js
+++ src/comps/Application.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
-import LandingPage   from './LandingPage'
+import React, { Component, lazy, Suspense } from 'react'
 import NavigationBar from './NavigationBar'
+
+const LandingPage = lazy(() => import('./LandingPage'));
 
 export default class Application extends Component {
   render() {
     return(
       <main id="UI">
         <NavigationBar />
-        <LandingPage />
+        <Suspense fallback={ <div><h3>Loading...</h3></div> }>
+          <LandingPage />
+        </Suspense>
       </main>
     )
   }
