80a816c05e57cf237978b6013bb7f66c8d4bb6f6
Cloud <cloud@kevia.me>
2018-12-16 15:52:08 -0700

    built ul for contacts

--- src/comps/LandingPage.js
+++ src/comps/LandingPage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import imgSrc from 'images/hero_background.jpg'
 
 export default function LandingPage() {
-  const [count, setCount] = useState(0)
 
   return(
     <div id='landing-page'>
@@ -16,10 +15,26 @@ export default function LandingPage() {
       </div>
       <div className="row nomargin bottom-half">
         <div className="col s12 m6 bio">
-          <h3 className="white-text">{`The count is ${count}`}</h3>
+
         </div>
         <div className="col s12 m6 contact">
-          <button className="waves-effect waves-light btn-large red" onClick={ () => setCount(count + 1) }>Click Me</button>
+          <ul>
+            <li>
+              <div className="padded-multiline">
+                <a href="#">linkedin</a>
+              </div>
+            </li>
+            <li>
+              <div className="padded-multiline">
+                <a>twitter</a>
+              </div>
+            </li>
+            <li>
+              <div className="padded-multiline">
+                <a>github</a>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
