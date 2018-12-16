0fc9beca731ce8fdddbe21ca0f71061427d006ab
Cloud <cloud@kevia.me>
2018-12-16 16:23:36 -0700

    getting somewhere

--- src/comps/LandingPage.js
+++ src/comps/LandingPage.js
@@ -5,33 +5,32 @@ export default function LandingPage() {
 
   return(
     <div id='landing-page'>
-      <div className="row top-half" style={{ backgroundImage: `url(${imgSrc})` }}>
-        <div className="tint-box"></div>
+      <div className="row top-half">
         <div className="col s12 text-box">
           <p className="title-name">Kevia Cloud</p>
           <h3>Software Engineer</h3>
           <div className="divider"></div>
         </div>
       </div>
-      <div className="row nomargin bottom-half">
+      <div className="row bottom-half">
         <div className="col s12 m6 bio">
-
+          <p>Currently working with React, Parcel, GraphQL, serverless and Google Cloud Functions. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
         </div>
         <div className="col s12 m6 contact">
           <ul>
             <li>
               <div className="padded-multiline">
-                <a href="#">linkedin</a>
+                <a href="https://www.linkedin.com/in/kevia-cloud/" target="_blank">linkedin</a>
               </div>
             </li>
             <li>
               <div className="padded-multiline">
-                <a>twitter</a>
+                <a href="https://twitter.com/keviaDotMe" target="_blank">twitter</a>
               </div>
             </li>
             <li>
               <div className="padded-multiline">
-                <a>github</a>
+                <a href="https://github.com/jhamPac" target="_blank">github</a>
               </div>
             </li>
           </ul>
