bb1d8d2025e5194f59bf1d71167122165d8a3700
jhamPac <cloud@kevia.me>
2019-02-05 18:46:27 -0700

    make x footer year dynamic

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { withRouter } from 'react-router'
 
-export default function Footer() {
-  const today = new Date().getFullYear()
+function Footer({ location }) {
+  const date = (location.pathname === '/1717') ? '17:17' : new Date().getFullYear()
 
   return(
     <nav id="footer" className="row">
       <div className="col s12">
-        <h5>{ today }</h5>
+        <h5>{date}</h5>
       </div>
     </nav>
   )
 }
+
+export default withRouter(Footer);
