1a7ed0417e40b8d1161af46ebb353bc5be8a513e
jhamPac <cloud@kevia.me>
2019-02-09 16:27:08 -0700

    converted the row into view-box

--- src/comps/Shmule.js
+++ src/comps/Shmule.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Shmule() {
   return (
     <div id="shmule">
-      <div className="row">
-        <div className="col s12 view-box">
+      <div className="row view-box">
+        <div className="col s12">
           <iframe src="https://www.smule.com/recording/brenda-russell-piano-in-the-dark/1021834707_2774143611/frame/box"></iframe>
         </div>
       </div>
