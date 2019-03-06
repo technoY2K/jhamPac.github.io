6bfa400312fb65100c6618c77e31d72789708f2e
jhamPac <cloud@kevia.me>
2019-03-05 19:20:21 -0700

    uh oh, getting it

--- src/comps/LandingPage.js
+++ src/comps/LandingPage.js
@@ -2,10 +2,18 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
+  const [ degrees, setDegree ] = useState(0)
+  const turnLeft  = () => setDegree(degrees - 90)
+  const turnRight = () => setDegree(degrees + 90)
+
+  const styleObj = {
+    transform: `translateZ(-100px) rotateY(${degrees}deg)`
+  }
+
   return(
     <div id='landing-page' className="container">
       <div className="wrap">
-	       <div className="cube">
+	       <div className="cube" style={styleObj}>
 		      <div className="front"></div>
 		      <div className="back"></div>
 		      <div className="top"></div>
@@ -14,6 +22,10 @@ export default function LandingPage() {
 		      <div className="right"></div>
 	      </div>
       </div>
+      <section>
+        <button onClick={ turnLeft }>Left</button>
+        <button onClick={ turnRight }>Right</button>
+      </section>
     </div>
   )
 }
