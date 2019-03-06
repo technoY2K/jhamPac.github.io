18b99d33157f1722bc988420c661b2e0c604267d
jhamPac <cloud@kevia.me>
2019-03-05 19:05:57 -0700

    adding some perspective

--- src/comps/LandingPage.js
+++ src/comps/LandingPage.js
@@ -4,46 +4,15 @@ import { Link } from 'react-router-dom'
 export default function LandingPage() {
   return(
     <div id='landing-page' className="container">
-      <div className="row top-half">
-        <div className="col s12 text-box">
-          <p className="title-name">Kevia Cloud</p>
-          <h3>Software Engineer</h3>
-          <div className="divider"></div>
-        </div>
-      </div>
-      <div className="row bottom-half">
-        <div className="col s12 m6 bio">
-          <p>Currently working with:</p>
-          <p>React 🥇</p>
-          <p>Parcel 🥇</p>
-          <p>GraphQL 🥈</p>
-          <p>Firebase 🥈</p>
-          <span>I love movies from the 1990s like The 5th Element and even terrible ones like Demolition Man. Prada > Gucci right now and Mokba is the jamz!</span>
-        </div>
-        <div className="col s12 m6 contact">
-          <ul>
-            <li>
-              <div className="padded-multiline purple darken-4">
-                <a href="https://www.linkedin.com/in/kevia-cloud/" target="_blank">linkedin</a>
-              </div>
-            </li>
-            <li>
-              <div className="padded-multiline purple darken-4">
-                <a href="https://twitter.com/keviaDotMe" target="_blank">twitter</a>
-              </div>
-            </li>
-            <li>
-              <div className="padded-multiline purple darken-4">
-                <a href="https://github.com/jhamPac" target="_blank">github</a>
-              </div>
-            </li>
-            <li>
-              <div className="padded-multiline black center-align">
-                <Link to="/1717">17:17</Link>
-              </div>
-            </li>
-          </ul>
-        </div>
+      <div className="wrap">
+	       <div className="cube">
+		      <div className="front"></div>
+		      <div className="back"></div>
+		      <div className="top"></div>
+		      <div className="bottom"></div>
+		      <div className="left"></div>
+		      <div className="right"></div>
+	      </div>
       </div>
     </div>
   )
