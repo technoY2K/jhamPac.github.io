1e58260374a129edb82432ed6b2ffb3821b53f86
jhamPac <cloud@kevia.me>
2019-03-09 15:18:32 -0700

    added content on the back cube

--- src/comps/LandingPage.js
+++ src/comps/LandingPage.js
@@ -23,8 +23,12 @@ export default function LandingPage() {
           <div className="left" style={{backgroundImage: `url(${twoSrc})`}}></div>
 		      <div className="back" style={{backgroundImage: `url(${threeSrc})`}}></div>
           <div className="right">
-            <p>Contact me Kevia Cloud</p>
-            <p>Blahjas adsfasdf asdfasdf</p>
+            <section>
+              <p>Contact Me:</p>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </section>
           </div>
 		      <div className="top"></div>
 		      <div className="bottom"></div>
